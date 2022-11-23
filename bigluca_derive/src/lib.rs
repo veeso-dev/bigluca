bc7246e09c47c8314ed36c8b9590e139d0d0f31a
veeso <christian.visintin1997@gmail.com>
2022-11-23 16:03:37 +0100

    ValidateAllPaths and ValidateAllFields for config

--- bigluca_derive/src/lib.rs
+++ bigluca_derive/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::{self, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, FieldsNamed};
 
 #[proc_macro_derive(AllVariants)]
 pub fn all_attributes(input: TokenStream) -> TokenStream {
@@ -29,4 +29,78 @@ pub fn all_attributes(input: TokenStream) -> TokenStream {
     } else {
         panic!("AllVariants must be derived by a `Enum`")
     }
+}
+
+#[proc_macro_derive(ValidateAllPaths)]
+pub fn validate_all_paths(input: TokenStream) -> TokenStream {
+    let DeriveInput { ident, data, .. } = parse_macro_input!(input);
+    let name = &ident;
+
+    let dt_struct = match data {
+        syn::Data::Struct(s) => s,
+        _ => panic!("ValidateAllPaths must be derived by a `Struct`"),
+    };
+
+    let mut tokens = Vec::new();
+
+    match dt_struct.fields {
+        syn::Fields::Named(FieldsNamed { named, .. }) => {
+            for field in named.iter() {
+                let field_name = field.ident.as_ref().unwrap();
+                tokens.push(quote! {
+                    if !self.#field_name.exists() { return anyhow::bail!("pussy does not exist") }
+                });
+            }
+        }
+        _ => panic!("struct {} does not contain named fields", ident),
+    }
+    // Implement ValidateAllPaths for type
+    let output = quote! {
+        impl Validate for #name {
+            fn validate(&self) -> anyhow::Result<()> {
+                #(#tokens)*
+
+                Ok(())
+            }
+        }
+    };
+
+    output.into()
+}
+
+#[proc_macro_derive(ValidateAllFields)]
+pub fn validate_all_fields(input: TokenStream) -> TokenStream {
+    let DeriveInput { ident, data, .. } = parse_macro_input!(input);
+    let name = &ident;
+
+    let dt_struct = match data {
+        syn::Data::Struct(s) => s,
+        _ => panic!("ValidateAllFields must be derived by a `Struct`"),
+    };
+
+    let mut tokens = Vec::new();
+
+    match dt_struct.fields {
+        syn::Fields::Named(FieldsNamed { named, .. }) => {
+            for field in named.iter() {
+                let field_name = field.ident.as_ref().unwrap();
+                tokens.push(quote! {
+                    self.#field_name.validate()?;
+                });
+            }
+        }
+        _ => panic!("struct {} does not contain named fields", ident),
+    }
+    // Implement ValidateAllFields for type
+    let output = quote! {
+        impl Validate for #name {
+            fn validate(&self) -> anyhow::Result<()> {
+                #(#tokens)*
+
+                Ok(())
+            }
+        }
+    };
+
+    output.into()
 }
